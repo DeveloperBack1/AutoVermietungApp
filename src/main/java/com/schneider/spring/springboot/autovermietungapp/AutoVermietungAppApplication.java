67c44087dfddf874bacbba094e7f99794b6f8611
Max50204 <schneidermaxim45@yahoo.com>
2025-01-27 12:50:11 +0100

    create project

---
+++ src/main/java/com/schneider/spring/springboot/autovermietungapp/AutoVermietungAppApplication.java
@@ -0,0 +1,13 @@
+package com.schneider.spring.springboot.autovermietungapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoVermietungAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoVermietungAppApplication.class, args);
+    }
+
+}
